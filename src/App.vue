395f573245a8fda11940bf48281f7f964c96e1eb
ldmk@outlook.com <ldmk@outlook.com>
2021-09-22 18:42:31 +0200

    First ocmmit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="dottxt logo" src="./assets/dottxt.png">
+    <Dottxt msg="Dottxt Text Editor"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Dottxt from './components/Dottxt.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        Dottxt,
+    },
+    methods: {
+        saveText: function() {
+            alert('saveText');
+        }
+    },
+    data(){
+        return {
+            //Nothing yet
+    }
 }
+};
+
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
+
 </style>
